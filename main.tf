7545001449db6090a183c347e7be8ed93bd22037
Matt Ashby <matt@ashbys.nz>
2020-03-26 22:28:47 +1300

    updated azurerm provider to v2.0.0 added mod level2_region1

--- main.tf
+++ main.tf
@@ -5,9 +5,11 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "<= 1.43.0"
+  # version = "<= 1.43.0"
+  version = "= 2.0.0"
+  features {}
 
-# Required if deploying via service principal
+  # Required if deploying via service principal
   # subscription_id = var.subscription_id
   # client_id       = var.client_id
   # client_secret   = var.client_secret
@@ -22,6 +24,9 @@ data "terraform_remote_state" "landingzone_hub" {
     resource_group_name  = var.lowerlevel_resource_group_name
     key                  = var.lowerlevel_key
   }
+}
+
+data "azurerm_client_config" "current" {
 }
 
 module "governance" {
@@ -44,11 +49,13 @@ module "level0_region1" {
   vnet_suffix = var.vnet_suffix
   nsg_suffix  = var.nsg_suffix
   rt_suffix   = var.rt_suffix
+  ip_suffix   = var.ip_suffix
 
   networking_object    = var.networking_object
   resource_groups      = var.resource_groups
   diagnostics_object   = var.diagnostics_object
   log_analytics_object = var.log_analytics_object
+  IP_address_object    = var.IP_address_object
 }
 
 module "level1_region1" {
@@ -57,4 +64,28 @@ module "level1_region1" {
   level0_NSG    = module.level0_region1.nsgs
   level0_subnet = module.level0_region1.subnets
   level0_rt     = module.level0_region1.route_table_obj
+  level0_rg     = module.level0_region1.resource_groups
+  tenant_id     = data.azurerm_client_config.current.tenant_id
+  object_id     = data.azurerm_client_config.current.object_id
+
+  rsv_suffix       = var.rsv_suffix
+  key_vault_suffix = var.key_vault_suffix
+
+  rsv_object       = var.rsv_object
+  key_vault_object = var.key_vault_object
+}
+
+module "level2_region1" {
+  source = "../tf-mod-leveltwo"
+
+  level0_vnet                 = module.level0_region1.virtual_networks
+  level0_subnet               = module.level0_region1.subnets
+  governance_storage_accounts = module.governance.storage_account_obj
+
+  vm_suffix      = var.vm_suffix
+  os_disk_suffix = var.os_disk_suffix
+  disk_suffix    = var.disk_suffix
+  nic_suffix     = var.nic_suffix
+
+  vm_object = var.vm_object
 }
--- provisioning.sh
+++ provisioning.sh
@@ -21,7 +21,7 @@
 # ## Get storage account key
 # ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
 
-# ### Create blob container
+# ## Create blob container
 # az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
 
 # echo "resource_group_name: $RESOURCE_GROUP_NAME" \
